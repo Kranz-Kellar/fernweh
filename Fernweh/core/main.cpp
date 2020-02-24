d83174aa19eb1c0b65b149e588bbe3eb2121781e
Andrew <kranz.kellar@gmail.com>
2020-02-24 10:30:45 +0300

    Added SFML

--- Fernweh/core/main.cpp
+++ Fernweh/core/main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
 #include "entities/Human.h"
 
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
 int main() {
 	std::cout << "Hello, Fernweh!" << std::endl;
 	Fernweh::Human hm;
+
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Test");
+	
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.display();
+	}
+
 	return 0;
 }
